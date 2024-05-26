57d6e97382414cc0b3977c8873ac535d3f2b3680
MucheruM <petermucheru420@gmail.com>
2024-05-26 14:35:32 -0700

    Edit the except:Exception code to remove empty error

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Test BaseModel for expected behavior and documentation"""
-from datetime import datetime
 import inspect
 import models
 import pep8 as pycodestyle
 import time
+from datetime import datetime
 import unittest
 from unittest import mock
 BaseModel = models.base_model.BaseModel
